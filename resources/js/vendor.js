3125f0c026da216b5d51c5736f6244d3617c8d48
Marcos Gomes Neto <marcosgomesneto>
2020-05-18 18:47:40 +0000

    Update

--- resources/js/vendor.js
+++ resources/js/vendor.js
@@ -3,5 +3,6 @@ var modernizr = require('./modernizr-webp.js');
 window.jQuery = jQuery;
 window.WOW = require('wowjs').WOW;
 
-var easing = require('jquery.easing')
+var easing = require('jquery.easing');
 var bootstrap = require('bootstrap');
+var particles = require('particles.js');
